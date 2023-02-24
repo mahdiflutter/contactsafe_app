3b1d6d1b1999f37cc17b8d054135ddeeb46ccf47
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-24 15:30:23 +0330

    Create maincolors_constant.dart

---
+++ contactsafe/lib/constants/maincolors_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static const mainBlue = Color(0xff3B5EDF);
+  static const mainGaryText = Color(0xff858585);
+  static const mainRed = Color(0xffD02026);
+  static const mainGreen = Color(0xff1DB68B);
+}
